bdca08c24de119494dbc845f1d230ca1a575fa0f
SpieczonyTost <pawel.ab@o2.pl>
2023-11-07 12:29:34 +0100

    changing playbook

--- venv/runME.bash
+++ venv/runME.bash
@@ -7,4 +7,4 @@ rm ~/.ssh/known_hosts.old
 ssh -o StrictHostKeyChecking=no vagrant@192.168.50.100 -i ~/.ssh/rsa_key
 ssh -o StrictHostKeyChecking=no vagrant@192.168.50.101 -i ~/.ssh/rsa_key
 
-ansible-playbook -i ~/Desktop/venv/wpInstall/inventory.yml ~/Desktop/venv/wpInstall/playbook.yml
+ansible-playbook -i ~/Desktop/Project/venv/wpInstall/inventory.yml ~/Desktop/Project/venv/wpInstall/playbook.yml
