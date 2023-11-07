6115f250b38729adb36a37920015afe50119bb00
SpieczonyTost <pawel.ab@o2.pl>
2023-11-07 13:08:33 +0100

    changing playbook

--- venv/runME.bash
+++ venv/runME.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
+first = $1
+
 vagrant up
 
 rm ~/.ssh/known_hosts
@@ -7,4 +9,8 @@ rm ~/.ssh/known_hosts.old
 ssh -o StrictHostKeyChecking=no vagrant@192.168.50.100 -i ~/.ssh/rsa_key
 ssh -o StrictHostKeyChecking=no vagrant@192.168.50.101 -i ~/.ssh/rsa_key
 
+if [ $first == 1 ]; then
 ansible-playbook -i ~/Desktop/Project/venv/wpInstall/inventory.yml ~/Desktop/Project/venv/wpInstall/playbook.yml
+else
+echo "siedem"
+fi
