b07895b0fe057d6097543c8273367de1054fb006
Pawel <70841950+SpieczonyTost@users.noreply.github.com>
2023-11-08 09:16:48 +0100

    changing if function (#6)

--- runME.bash
+++ runME.bash
@@ -9,12 +9,5 @@ rm ~/.ssh/known_hosts.old
 ssh -o StrictHostKeyChecking=no vagrant@192.168.50.100 -i ~/.ssh/rsa_key
 ssh -o StrictHostKeyChecking=no vagrant@192.168.50.101 -i ~/.ssh/rsa_key
 
-if [ $first == 1 ]; then
-
 ansible-playbook -i python3Install/inventoryPy.yml python3Install/playbookPy.yml -e 'ansible_python_interpreter=/usr/bin/python2'
-
 ansible-playbook -i wpInstall/inventory.yml wpInstall/playbook.yml -e 'ansible_python_interpreter=/usr/bin/python3'
-
-else
-ansible-playbook -i wpInstall/inventory.yml wpInstall/playbook.yml
-fi
