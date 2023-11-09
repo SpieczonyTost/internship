f72036afb70c619d7622174037e0a82158e1966d
Pawel <70841950+SpieczonyTost@users.noreply.github.com>
2023-11-09 10:55:26 +0100

    changed files and added 3 files to prometheus role (#17)

--- runME.bash
+++ runME.bash
@@ -8,6 +8,8 @@ rm ~/.ssh/known_hosts.old
 
 ssh -o StrictHostKeyChecking=no vagrant@192.168.50.100 -i ~/.ssh/rsa_key
 ssh -o StrictHostKeyChecking=no vagrant@192.168.50.101 -i ~/.ssh/rsa_key
+ssh -o StrictHostKeyChecking=no vagrant@192.168.50.102 -i ~/.ssh/rsa_key
+ssh -o StrictHostKeyChecking=no vagrant@192.168.50.103 -i ~/.ssh/rsa_key
 
-ansible-playbook -i python3Install/inventoryPy.yml python3Install/playbookPy.yml -e 'ansible_python_interpreter=/usr/bin/python2'
-ansible-playbook -i wpInstall/inventory.yml wpInstall/playbook.yml -e 'ansible_python_interpreter=/usr/bin/python3'
+ansible-playbook -i python3Install/inventoryPy.yml python3Install/playbookPy.yml
+ansible-playbook -i wpInstall/inventory.yml wpInstall/playbook.yml
