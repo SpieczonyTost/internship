e2c85a918856bf18173d269ab8b715a1ea996b78
SpieczonyTost <pawel.ab@o2.pl>
2023-11-13 18:07:45 +0100

    tags and checking if installed

--- runME.bash
+++ runME.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-vagrant up ubuntu ubuntuProm
+vagrant up
 
 rm ~/.ssh/known_hosts
 rm ~/.ssh/known_hosts.old
 
-# ssh -o StrictHostKeyChecking=no vagrant@192.168.50.100 -i ~/.ssh/rsa_key
+ssh -o StrictHostKeyChecking=no vagrant@192.168.50.100 -i ~/.ssh/rsa_key
 ssh -o StrictHostKeyChecking=no vagrant@192.168.50.101 -i ~/.ssh/rsa_key
-# ssh -o StrictHostKeyChecking=no vagrant@192.168.50.102 -i ~/.ssh/rsa_key
+ssh -o StrictHostKeyChecking=no vagrant@192.168.50.102 -i ~/.ssh/rsa_key
 ssh -o StrictHostKeyChecking=no vagrant@192.168.50.103 -i ~/.ssh/rsa_key
 
-# ansible-playbook -i python3Install/inventoryPy.yml python3Install/playbookPy.yml
+ansible-playbook -i python3Install/inventoryPy.yml python3Install/playbookPy.yml
 ansible-playbook -i wpInstall/inventory.yml wpInstall/playbook.yml
