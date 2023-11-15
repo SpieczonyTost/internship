96e877cacb8d9016eac4f22191183e05825c3a55
Mikołaj Zaśko <mikolaj.zasko890290@gmail.com>
2023-11-15 12:17:53 +0100

    run me now deletes known_hosts

--- runME.bash
+++ runME.bash
@@ -2,8 +2,8 @@
 
 vagrant up
 
-# rm ~/.ssh/known_hosts
-# rm ~/.ssh/known_hosts.old
+rm ~/.ssh/known_hosts
+rm ~/.ssh/known_hosts.old
 
 # ssh -o StrictHostKeyChecking=no vagrant@192.168.50.100 -i ~/.ssh/rsa_key
 # ssh -o StrictHostKeyChecking=no vagrant@192.168.50.101 -i ~/.ssh/rsa_key
