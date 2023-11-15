388d4b034337b9919325c6a931af022ae2f7911a
SpieczonyTost <pawel.ab@o2.pl>
2023-11-15 13:53:10 +0100

    crontab

--- workingCron.bash
+++ workingCron.bash
@@ -5,3 +5,14 @@ touch /cronTest/workingCron
 bash cronWeekly.bash
 touch /cronTest/workingCronFull
 bash cronWeeklyFull.bash
+
+
+
+# 0 20 1 1 * bash cronFoldersYear.bash
+# 0 20 1 * * bash cronFoldersMonth.bash
+# 0 20 1 * * bash cronFOM.bash
+# 0 20 * * 1 bash cronWeekly.bash
+# 0 20 * * 3 bash cronWeekly.bash
+# 0 20 * * 5 bash cronWeekly.bash
+# 0 20 * * 7 bash cronWeeklyFull.bash
+# crontab -e
